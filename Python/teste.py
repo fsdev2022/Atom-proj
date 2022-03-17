001a65cf5f705329f98922f5fbdd4061c2289b9e
FSdev2022 <fsdev2022@gmail.com>
2022-03-17 19:08:30 -0300

    manda ver

---
+++ Python/teste.py
@@ -0,0 +1,66 @@
+oi self.assert_(
+
+, 'message')
+
+
+
+if __name__ == '__main__':
+    main()
+    self.assertFalse(actual, 'message')
+    try:
+        pass
+    except Exception as e:
+        raise
+    else:
+        pass
+        # IDEA: testando
+        # QUESTION:
+        while a=1:
+            pass
+
+            #!/usr/bin/env python
+            #!/usr/bin/env python3
+            # -*- coding: utf-8 -*-
+            # REVIEW:
+            import rpdb2; rpdb2.start_embedded_debugger('debug_password')
+            # TEMP:
+            # TODO:
+            try:
+                pass
+            except Exception as e:
+                raise
+
+                try:
+                    pass
+                except Exception as e:
+                    raise
+                else:
+                    pass
+
+                    try:
+                        pass
+                    except Exception as e:
+                        raise
+                    finally:
+                        pass
+
+                        def foo():
+                            doc = "The  property."
+                            def fget(self):
+                                return self._
+                            def fset(self, value):
+                                self._ = value
+                            def fdel(self):
+                                del self._
+                            return locals()
+                         = property(**())
+
+                         import pudb; pudb.set_trace()
+
+                         [value for value in variable]
+
+                         def fname(arg):
+                             pass
+
+                             def mname(self, arg):
+                                 pass
